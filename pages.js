c2dc13bcbaaa8ec46ecf7f53e953e590e0fabf71
Felipe Affonso <felipe-affonso@hotmail.com>
2020-12-13 22:57:26 -0300

    Changing pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/hashirshoaeb/hashirshoaeb.github.io.git";
+const repoURL = "https://github.com/felipe-affonso/felipe-affonso.github.io.git";
 
 ghpages.publish(
   pathname,
